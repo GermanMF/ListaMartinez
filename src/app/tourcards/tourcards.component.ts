4e0206468e602d0529e9ee77f94cae66158e135c
GermanMF <gmartinezfragoso@gmail.com>
2023-03-16 00:46:03 -0600

    Pushing the latest changes for revision

---
+++ src/app/tourcards/tourcards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tourcards',
+  templateUrl: './tourcards.component.html',
+  styleUrls: ['./tourcards.component.css']
+})
+export class TourcardsComponent {
+
+}
